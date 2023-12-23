e0de963c45c2f84330013a5f7350205539a44177
Panainte Diana <panaintediana29@gmail.com>
2023-12-23 14:26:37 +0200

    photon voice, nickname, added fbx to unity

--- CarturestiCarusel3.0/Assets/RoomManager.cs
+++ CarturestiCarusel3.0/Assets/RoomManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using System.Diagnostics;
 using System.Security.Cryptography;
@@ -9,13 +10,17 @@ public class RoomManager : MonoBehaviourPunCallbacks
 {
 
     public GameObject player;
+
     public Transform spawnPoint;
+
+    public InputField playerNickname;
+
+    public GameObject nicknameInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        UnityEngine.Debug.Log("Connecting...");
-        PhotonNetwork.ConnectUsingSettings();
+        
     }
 
     public override void OnConnectedToMaster()
@@ -45,5 +50,13 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
         GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
         _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+    }
+
+    public void StartTheGame()
+    {
+        PhotonNetwork.NickName = playerNickname.text;
+        UnityEngine.Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
+        nicknameInput.SetActive(false);
     }
 }
